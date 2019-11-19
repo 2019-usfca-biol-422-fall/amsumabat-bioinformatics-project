689fb14c8e3fd934a11c93ff861396e0311d882b
Adelle Sumabat <amsumabat@dons.usfca.edu>
2019-11-19 06:32:54 +0000

    Add Script 05

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -1,2 +1,8 @@
 # use cut, sort, and uniq -c to help you summarize the results from the
 # BLAST search.
+
+for blastresults in /data/my-illumina-sequences/blast_output/*.csv
+do
+echo processing $blastresults
+cut -d, -f1 $blastresults | sort | uniq -c | sort -n > /data/my-illumina-sequences/blast_results_summarization/$(basename -s .csv $blastresults)_summarized.csv
+done
