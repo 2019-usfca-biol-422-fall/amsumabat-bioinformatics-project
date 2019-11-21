146b9b4d3f140055ae84863bbd6df52f091337bf
Adelle Sumabat <amsumabat@dons.usfca.edu>
2019-11-21 04:10:15 +0000

    Edit script

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -7,5 +7,5 @@ for untrimmed_fastq_files in /data/my-illumina-sequences/unzipped/AS*.fastq
 do
 echo processing "$untrimmed_fastq_files..."
 TrimmomaticSE -threads 4 -phred33 "$untrimmed_fastq_files" /data/my-illumina-sequences/trimmed/"$(basename -s .fastq "$untrimmed_fastq_files")".trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:140
-echo completed trimming $untrimmed_fastq_files
+echo completed trimming "$untrimmed_fastq_files"
 done
