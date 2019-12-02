ea80f80644a851b3891dd3fe8084aa6ee23ed4f2
Adelle Sumabat <amsumabat@usfca.edu>
2019-12-01 21:02:45 -0800

    Edit script 05 to analyze BLAST results overall not individually

--- scripts/05_summarize_blast_results.sh
+++ scripts/05_summarize_blast_results.sh
@@ -3,10 +3,4 @@
 # use cut, sort, and uniq -c to help you summarize the results from the
 # BLAST search.
 
-for blastresults in /data/my-illumina-sequences/blast_output/*.csv
-do
-echo processing "$blastresults"
-cut -d, -f1 "$blastresults" | sort | uniq -c | sort -n > /data/my-illumina-sequences/blast_results_summarization/"$(basename -s .csv "$blastresults")"_summarized.csv
-done
-
-cut -d, -f1 /data/my-illumina-sequences/blast_output/*.csv | sort | uniq -c | sort -n
+cut -d, -f1 /data/my-illumina-sequences/blast_output/*.csv | sort | uniq -c | sort -n > /data/my-illumina-sequences/blast_results_overall
